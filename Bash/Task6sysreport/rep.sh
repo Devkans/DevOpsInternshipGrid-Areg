640d877ab5fc5609f2fbd681c8c59615f8ede8bb
areggevorgyann16 <areggevorgyann16@gmail.com>
2025-07-11 22:24:58 +0400

    task6 tested fixed mhz

--- Bash/Task6sysreport/rep.sh
+++ Bash/Task6sysreport/rep.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
 REPORT_FILE="system_report.txt"
 
-# Overwrite the file
+# title
 echo "===== System Report =====" > "$REPORT_FILE"
 
-echo "Current date and time      : $(date)" >> "$REPORT_FILE"
-echo "Current user        : $(whoami)" >> "$REPORT_FILE"
+#  info
+echo "Date and Time       : $(date)" >> "$REPORT_FILE"
+echo "Current User        : $(whoami)" >> "$REPORT_FILE"
 echo "Hostname            : $(hostname)" >> "$REPORT_FILE"
-echo "Internal IP address : $(hostname -I | awk '{print $1}')" >> "$REPORT_FILE"
-echo "External IP address : $(curl -s ifconfig.me || echo 'Unavailable')" >> "$REPORT_FILE"
-echo "Distribution        : $(source /etc/os-release && echo $NAME $VERSION)" >> "$REPORT_FILE"
+echo "Internal IP Address : $(hostname -I | awk '{print $1}')" >> "$REPORT_FILE"
+echo "External IP Address : $(curl -s ifconfig.me || echo 'Unavailable')" >> "$REPORT_FILE"
+echo "Distribution        : $(source /etc/os-release && echo "$NAME $VERSION")" >> "$REPORT_FILE"
 echo "System Uptime       : $(uptime -p)" >> "$REPORT_FILE"
 
 echo "" >> "$REPORT_FILE"
@@ -18,10 +18,11 @@ echo "" >> "$REPORT_FILE"
 echo "Disk Usage on '/'   : Used: $(df -h / | awk 'NR==2 {print $3}'), Free: $(df -h / | awk 'NR==2 {print $4}')" >> "$REPORT_FILE"
 echo "RAM Info            : Total: $(free -h | awk '/^Mem:/ {print $2}'), Free: $(free -h | awk '/^Mem:/ {print $4}')" >> "$REPORT_FILE"
 echo "CPU Cores           : $(nproc)" >> "$REPORT_FILE"
-echo "CPU mhz            : $(lscpu | grep "CPU MHz"
+echo "CPU mhz             : $(grep "cpu MHz" /proc/cpuinfo | head -n 1 | awk '{print $4, "MHz"}')" >> "$REPORT_FILE"
 
 echo "" >> "$REPORT_FILE"
 
+# Append the footer
 echo "=========================" >> "$REPORT_FILE"
 
-echo "System report saved to '$REPORT_FILE'"
+echo "System report saved to $REPORT_FILE"
